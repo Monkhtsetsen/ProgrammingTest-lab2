93d795ab5087ac35172251e3c311ad2548ba5c10
Munkhtsetsen <munkhtsetsen.tumennasan@unimedia.mn~>
2026-09-10 14:11:58 +0800

    alhan 3 buyu stagestei run hiisen

---
+++ stages_script.js
@@ -0,0 +1,15 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+export const options = {
+  stages: [
+    { duration: "30s", target: 5 }, // halaalt
+    { duration: "1m", target: 30 }, // usgult
+    { duration: "30s", target: 100 }, // orgil
+    { duration: "30s", target: 0 }, // buult
+  ],
+};
+export default function () {
+  const res = http.get("https://test.k6.io");
+  check(res, { "status 200 baina": (r) => r.status === 200 });
+  sleep(1);
+}
