ed296b05c5fe5b81226fffadd68ffab9e7defe0b
Munkhtsetsen <munkhtsetsen.tumennasan@unimedia.mn~>
2026-09-10 13:31:03 +0800

    anhnii test

---
+++ script.js
@@ -0,0 +1,8 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+export const options = { vus: 5, duration: "30s" };
+export default function () {
+  const res = http.get("https://test/k6.io");
+  check(res, { "status 200 baina": (r) => r.status === 200 });
+  sleep(1);
+}
