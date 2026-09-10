d19ee87a097b2562b7df89998f1d7f3269879fe8
Munkhtsetsen <munkhtsetsen.tumennasan@unimedia.mn~>
2026-09-10 13:42:43 +0800

    url iig aldaatai basniig zasch oruulsan. Anhnii testiig dahin hiisen

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import http from "k6/http";
 import { sleep, check } from "k6";
 export const options = { vus: 5, duration: "30s" };
 export default function () {
-  const res = http.get("https://test/k6.io");
+  const res = http.get("https://test.k6.io");
   check(res, { "status 200 baina": (r) => r.status === 200 });
   sleep(1);
 }
