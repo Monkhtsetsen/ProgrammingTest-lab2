cb05a8d03920b6efe979e51dedff1f8f437207db
Munkhtsetsen <munkhtsetsen.tumennasan@unimedia.mn~>
2026-09-11 10:55:18 +0800

    tailan

--- ai_script.js
+++ ai_script.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
-import encoding from "k6/encoding"; // <-- FIX: Added missing import
+import encoding from "k6/encoding";
 
 export const options = {
   stages: [
